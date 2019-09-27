cbe120e4d3c881062d110d29a58d5c74d96ffc68
Thomas Indrias <thomasindrias@Thomass-MBP.lan>
2019-09-27 17:58:35 +0200

    part1

---
+++ Labs/lab1/part1.py
@@ -0,0 +1,20 @@
+# Dependencies
+import pandas as pd
+import numpy as np
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import MinMaxScaler
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Load the train and test datasets to create two DataFrames
+train_url = "http://s3.amazonaws.com/assets.datacamp.com/course/Kaggle/train.csv"
+train = pd.read_csv(train_url)
+test_url = "http://s3.amazonaws.com/assets.datacamp.com/course/Kaggle/test.csv"
+test = pd.read_csv(test_url)
+
+print("***** Train_Set *****")
+print(train.head())
+print("\n")
+print("***** Test_Set *****")
+print(test.head())
