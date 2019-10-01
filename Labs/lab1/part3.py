efba2f2e5dbfd81c48a817e37895ab881bed4444
Thomas Indrias <thomasindrias@gmail.com>
2019-10-01 13:28:46 +0200

    figure plot

--- Labs/lab1/part3.py
+++ Labs/lab1/part3.py
@@ -1,10 +1,16 @@
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+from matplotlib.pyplot import matplotlib
+import pandas as pd
 from sklearn.datasets import load_breast_cancer
+import numpy as np
 
 
 cancer = load_breast_cancer()
 print(cancer.DESCR)
 
-print(len(cancer.data[cancer.target==1]))
+print(len(cancer.data[cancer.target == 1]))
 '''
 import numpy as np
 import matplotlib.pyplot as plt 
@@ -25,33 +31,33 @@ plt.tight_layout()# let's make good plots
 plt.show()
 '''
 
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt 
-from matplotlib.pyplot import matplotlib
-from sklearn.preprocessing import StandardScaler
-from sklearn.decomposition import PCA
-
-
-cancer_df=pd.DataFrame(cancer.data,columns=cancer.feature_names)# just convert the scikit learn data-set to pandas data-frame.
-plt.subplot(1,2,1)#fisrt plot
-plt.scatter(cancer_df['worst symmetry'], cancer_df['worst texture'], s=cancer_df['worst area']*0.05, color='magenta', label='check', alpha=0.3)
-plt.xlabel('Worst Symmetry',fontsize=12)
-plt.ylabel('Worst Texture',fontsize=12)
-plt.subplot(1,2,2)# 2nd plot
-plt.scatter(cancer_df['mean radius'], cancer_df['mean concave points'], s=cancer_df['mean area']*0.05, color='purple', label='check', alpha=0.3)
-plt.xlabel('Mean Radius',fontsize=12)
-plt.ylabel('Mean Concave Points',fontsize=12)
+'''
+# just convert the scikit learn data-set to pandas data-frame.
+cancer_df = pd.DataFrame(cancer.data, columns=cancer.feature_names)
+plt.subplot(1, 2, 1)  # fisrt plot
+plt.scatter(cancer_df['worst symmetry'], cancer_df['worst texture'],
+            s=cancer_df['worst area']*0.05, color='magenta', label='check', alpha=0.3)
+plt.xlabel('Worst Symmetry', fontsize=12)
+plt.ylabel('Worst Texture', fontsize=12)
+plt.subplot(1, 2, 2)  # 2nd plot
+plt.scatter(cancer_df['mean radius'], cancer_df['mean concave points'],
+            s=cancer_df['mean area']*0.05, color='purple', label='check', alpha=0.3)
+plt.xlabel('Mean Radius', fontsize=12)
+plt.ylabel('Mean Concave Points', fontsize=12)
 plt.tight_layout()
-#plt.show()
+plt.show()
+'''
 
-scaler=StandardScaler()#instantiate
-scaler.fit(cancer.data) # compute the mean and standard which will be used in the next command
-X_scaled=scaler.transform(cancer.data)# fit and transform can be applied together and I leave that for simple exercise
+scaler = StandardScaler()  # instantiate
+# compute the mean and standard which will be used in the next command
+scaler.fit(cancer.data)
+# fit and transform can be applied together and I leave that for simple exercise
+X_scaled = scaler.transform(cancer.data)
 # we can check the minimum and maximum of the scaled features which we expect to be 0 and 1
 print("after scaling minimum\n", X_scaled.min(axis=0))
 print('\n')
 
+'''
 #Fitting the PCA algorithm with our Data
 pca = PCA().fit(X_scaled)
 #Plotting the Cumulative Summation of the Explained Variance
@@ -61,17 +67,37 @@ plt.xlabel('Number of Components')
 plt.ylabel('Variance (%)') #for each component
 plt.title('Dataset Variance')
 plt.show()
+'''
 
-pca=PCA(n_components=3)
+pca = PCA(n_components=3)
 pca.fit(X_scaled)
-X_pca=pca.transform(X_scaled)
-print("shape of X_pca\n", X_pca.shape) # let's check the shape of X_pca array
+X_pca = pca.transform(X_scaled)
+print("shape of X_pca\n", X_pca.shape)  # let's check the shape of X_pca array
 print('\n')
 
-ex_variance=np.var(X_pca,axis=0)
+ex_variance = np.var(X_pca, axis=0)
 ex_variance_ratio = ex_variance/np.sum(ex_variance)
 print("Variance ratio\n", ex_variance_ratio)
 
+Xax = X_pca[:, 0]
+Yax = X_pca[:, 1]
+labels = cancer.target
+cdict = {0: 'red', 1: 'green'}
+labl = {0: 'Malignant', 1: 'Benign'}
+marker = {0: '*', 1: 'o'}
+alpha = {0: .3, 1: .5}
+fig, ax = plt.subplots(figsize=(7, 5))
+fig.patch.set_facecolor('white')
+for l in np.unique(labels):
+    ix = np.where(labels == l)
+    ax.scatter(Xax[ix], Yax[ix], c=cdict[l], s=40,
+               label=labl[l], marker=marker[l], alpha=alpha[l])
+# for loop ends
+plt.xlabel("First Principal Component", fontsize=14)
+plt.ylabel("Second Principal Component", fontsize=14)
+plt.legend()
+plt.show()
+# please check the scatter plot of the remaining component and you will understand the difference
 
 
 # *** QUESTIONS ***
@@ -79,11 +105,11 @@ print("Variance ratio\n", ex_variance_ratio)
 # 1. Can you choose n components=2? Can you think of some method to test this?
 #   > [0.70002815 0.29997185]
 #   > Choosing 2 components contributes the first component to have 70% of the total variance.
-# 
-# 2. Create the scatter plot of the third principal component (that is, you combine the third 
+#
+# 2. Create the scatter plot of the third principal component (that is, you combine the third
 #    principal component with the first and then the second principal component). What can
 #    you see with the plot? What is the difference?
-#   
+#
 #   > answer
 #
 # 3. Can you tell which feature contribute more towards the 1st PC?
